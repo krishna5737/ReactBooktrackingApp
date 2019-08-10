25b3c05566df1ca49462608336672ad63a686bf8
krishna5737 <iro2015007@iiita.ac.in>
2019-08-10 09:49:44 +0530

    Added Debounce in Search Query

--- src/BooksRelatedComponent/Search.js
+++ src/BooksRelatedComponent/Search.js
@@ -2,56 +2,52 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import * as BooksAPI from './../BooksAPI';
-
+import {debounce} from 'lodash';
 export default class Search extends Component {
-	state = {
-		books: []
-	};
-
-	resetSearchResults() {
-		this.setState({ books: [] });
-	}
-
-	searchBooks(query) {
-		const { booksOnShelf } = this.props;
-
-		if (query === '') {
-			this.resetSearchResults();
-		} else {
-			BooksAPI.search(query).then((books) => {
-				if (typeof books === 'undefined' || books.error === 'empty query')
-					return this.resetSearchResults();
-
-				books.map(function(book) {
-					var bookIndex = booksOnShelf.findIndex(function(bookOnShelf) {
-						return bookOnShelf.id === book.id;
-					});
-
-					if (bookIndex > -1) {
-						book.shelf = booksOnShelf[bookIndex].shelf;
-					} else {
-						book.shelf = 'none';
-					}
-
-					return book;
-				});
-				this.setState({ books });
-			});
-		}
-	}
-
-	render() {
-		const { bookShelfChangeStatus } = this.props;
-		return (
-			<div className="search-books">
-				<SearchBar handleSearchQuery={(query) => this.searchBooks(query)} />
-				<SearchResults
-					books={this.state.books}
-					bookShelfChangeStatus={function(book, newShelf) {
-						return bookShelfChangeStatus(book, newShelf);
-					}}
-				/>
-			</div>
-		);
-	}
+    state = {
+        books: []
+    };
+    resetSearchResults() {
+        this.setState({books: [] });
+    }
+    searchBooksRaw(query) {
+        const { booksOnShelf } = this.props;
+        if (query === '') {
+            this.resetSearchResults();
+        } else {
+            BooksAPI.search(query).then((books) => {
+                if (typeof books === 'undefined' || books.error === 'empty query')
+                    return this.resetSearchResults();
+                books.map(function(book) {
+                    var bookIndex = booksOnShelf.findIndex(function(bookOnShelf) {
+                        return bookOnShelf.id === book.id;
+                    });
+                    if (bookIndex > -1) {
+                        book.shelf = booksOnShelf[bookIndex].shelf;
+                    } else {
+                        book.shelf = 'none';
+                    }
+                    return book;
+                });
+                this.setState({books: books});
+            });
+        }
+    }
+    searchBooks = debounce(query => {
+        this.searchBooksRaw(query);
+    }, 500)
+    render() {
+        const { bookShelfChangeStatus } = this.props;
+        return (
+            <div className="search-books">
+                <SearchBar handleSearchQuery={(query) => this.searchBooks(query)} />
+                <SearchResults
+                    books={this.state.books}
+                    bookShelfChangeStatus={function(book, newShelf) {
+                        return bookShelfChangeStatus(book, newShelf);
+                    }}
+                />
+            </div>
+        );
+    }
 }
